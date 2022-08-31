8cadde6f8afa2e2f632ca0455972e0e2fa7ade2b
lov3five <luongtranstaff@gmail.com>
2022-08-31 18:26:20 +0700

    fix led

--- Lab2/Lab2.4_DHTSensor.py
+++ Lab2/Lab2.4_DHTSensor.py
@@ -21,6 +21,6 @@ DHT_PIN = 4 #GPIO4
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
-        print("Nhiet do = {0:0.1f}*C    Humidity={1:0.1f}%".format(temperature,humidity))
+        print("Temp={0:0.1f}*C    Humidity={1:0.1f}%".format(temperature,humidity))
     else:
         print("Error!!! WARNING DATA FROM SENSOR")
--- Lab2/Lab2.5_DHTSensor_Led.py
+++ Lab2/Lab2.5_DHTSensor_Led.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import Adafruit_DHT
+import Adafruit_DHT #library of sensor DHT
 
 GPIO.setmode(GPIO.BCM)
 RED_LED_PIN = 17 #GPIO = 11 BOARD
@@ -11,15 +11,21 @@ GPIO.setup(27, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
 DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 4 #GPIO4
+DHT_PIN = 4 #GPIO = 7 BOARD
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     if temperature > 27:
+        GPIO.output(GREEN_LED_PIN, GPIO.LOW)
+        GPIO.output(YELLOW_LED_PIN, GPIO.LOW)
         GPIO.output(RED_LED_PIN, GPIO.HIGH)
         print("Temp={0:0.1f}*C =>> RED turn on")
     elif temperature <= 27 and temperature >= 20:
+        GPIO.output(GREEN_LED_PIN, GPIO.LOW)
+        GPIO.output(RED_LED_PIN, GPIO.LOW)
         GPIO.output(YELLOW_LED_PIN, GPIO.HIGH)
         print("Temp={0:0.1f}*C =>> YELLOW turn on")
     elif temperature < 20:
+        GPIO.output(RED_LED_PIN, GPIO.LOW)
+        GPIO.output(YELLOW_LED_PIN, GPIO.LOW)
         GPIO.output(GREEN_LED_PIN, GPIO.HIGH)
         print("Temp={0:0.1f}*C =>> GREEN turn on")
