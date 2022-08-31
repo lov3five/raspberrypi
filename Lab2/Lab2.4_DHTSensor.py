8d7947a8cc76f70cbf42306e37306a3d09237aff
lov3five <luongtranstaff@gmail.com>
2022-08-31 11:39:24 +0700

    Update

---
+++ Lab2/Lab2.4_DHTSensor.py
@@ -0,0 +1,26 @@
+""" 
+    *Update*
+    sudo apt-get update
+    sudo apt-get install build-essential python-dev
+
+    * clone the Adafruit library *
+    git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+    cd Adafruit_Python_DHT
+
+    *  install the library for Python 2 and Python 3 *
+    sudo python setup.py install
+    sudo python3 setup.py install
+"""
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+
+GPIO.setmode(GPIO.BCM)
+
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4 #GPIO4
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        print("Nhiet do = {0:0.1f}*C    Humidity={1:0.1f}%".format(temperature,humidity))
+    else:
+        print("Error!!! WARNING DATA FROM SENSOR")
---
+++ Lab2/Lab2.5_DHTSensor_Led.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+
+GPIO.setmode(GPIO.BCM)
+RED_LED_PIN = 17 #GPIO = 11 BOARD
+GREEN_LED_PIN = 27 #GPIO = 13 BOARD
+YELLOW_LED_PIN = 22 #GPIO = 15 BOARD
+
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4 #GPIO4
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    if temperature > 27:
+        GPIO.output(RED_LED_PIN, GPIO.HIGH)
+        print("Temp={0:0.1f}*C =>> RED turn on")
+    elif temperature <= 27 and temperature >= 20:
+        GPIO.output(YELLOW_LED_PIN, GPIO.HIGH)
+        print("Temp={0:0.1f}*C =>> YELLOW turn on")
+    elif temperature < 20:
+        GPIO.output(GREEN_LED_PIN, GPIO.HIGH)
+        print("Temp={0:0.1f}*C =>> GREEN turn on")
